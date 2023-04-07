fb7579bb24d5b0d1dcd8f91754e52d3be26e5472
ZohanKPI <denizah4022@gmail.com>
2023-04-07 22:42:42 +0300

    Added list2 and comparison

--- py_charm.py
+++ py_charm.py
@@ -1,6 +1,12 @@
 def mane_function(x,y,z):
     list1 = [x,y,z]
     print(list1)
+    list2 = [z,y,x]
+    print(list2)
+    if list2 != list1:
+        print("Lists are not the same!")
+    else:
+        print("Lists are the same!")
     if x > y and x > z:
         return x
     elif y > x and y > z:
