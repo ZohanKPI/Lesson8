40c70fff01bf7f75391ba3e683226176cfaa691f
ZohanKPI <denizah4022@gmail.com>
2023-04-07 21:58:51 +0300

    Added randomized list2

--- mane.py
+++ mane.py
@@ -1,6 +1,15 @@
+import random
 def mane_function(list1):
     for x in list1:
         print(x)
-        
-my_list = [1,2,3]
-mane_function(my_list)
+
+
+my_list1 = [1,2,3]
+my_list2 = []
+for i in range (3):
+    my_list2.append(random.randint(1,5))
+
+mane_function(my_list1)
+mane_function(my_list2)
+
+
