c930754f91d86ca54983b0b1d183151d1d8e4e1b
ZohanKPI <denizah4022@gmail.com>
2023-04-07 23:02:12 +0300

    Went back to normal

--- py_charm.py
+++ py_charm.py
@@ -1,23 +1,15 @@
-def mane_function(x,y,z,k):
-    list1 = [x,y,z,k]
+def mane_function(x,y,z):
+    list1 = [x,y,z]
     print(list1)
-    list2 = [x,y,z,k]
+    list2 = [z,y,x]
     print(list2)
     if list2 != list1:
         print("Lists are not the same!")
     else:
         print("Lists are the same!")
-    if isinstance(x, int) and isinstance(y, int) and isinstance(z, int):
-        if x > y and x > z:
-            return x
-        elif y > x and y > z:
-            return y
-        else:
-            return z
+    if x > y and x > z:
+        return x
+    elif y > x and y > z:
+        return y
     else:
-        return None
-a = "Python"
-b = "Java"
-c = "C++"
-d = "JavaScript"
-print(mane_function(a,b,c,d))
+        return z
