7f5b8899d2e427df15f47ea3cfea79cffe049da5
ZohanKPI <denizah4022@gmail.com>
2023-04-07 22:58:30 +0300

    Added some bad code

--- py_charm.py
+++ py_charm.py
@@ -1,15 +1,23 @@
-def mane_function(x,y,z):
-    list1 = [x,y,z]
+def mane_function(x,y,z,k):
+    list1 = [x,y,z,k]
     print(list1)
-    list2 = [z,y,x]
+    list2 = [x,y,z,k]
     print(list2)
     if list2 != list1:
         print("Lists are not the same!")
     else:
         print("Lists are the same!")
-    if x > y and x > z:
-        return x
-    elif y > x and y > z:
-        return y
+    if isinstance(x, int) and isinstance(y, int) and isinstance(z, int):
+        if x > y and x > z:
+            return x
+        elif y > x and y > z:
+            return y
+        else:
+            return z
     else:
-        return z
+        return None
+a = "Python"
+b = "Java"
+c = "C++"
+d = "JavaScript"
+print(mane_function(a,b,c,d))
